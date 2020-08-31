3d742f466f45ce73c31b427cca8aa8ec4553b461
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-08-31 22:55:13 +0100

    Add tooltip to the 'copy' icon

--- src/Copy.js
+++ src/Copy.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FileCopyRoundedIcon from '@material-ui/icons/FileCopyRounded';
+import Tooltip from '@material-ui/core/Tooltip';
 import './Copy.css';
 
 export const Copy = props => {
+	const [tooltip, setTooltip] = useState('Copy');
+
 	const onClick = () => {
-		props.forwardedRef.current.select();
-		document.execCommand('copy'); // copy to the clipboard
+		if (props.forwardedRef.current.innerHTML) {
+			props.forwardedRef.current.select();
+			document.execCommand('copy'); // copy to the clipboard
+			setTooltip('Copied');
+		}
+	};
+
+	const onMouseLeave = () => {
+		setTooltip('Copy');
 	};
 
 	return (
-		<span className='copy'>
-			<FileCopyRoundedIcon onClick={onClick} className='copy_icon' />
+		<span className='copy' onMouseLeave={onMouseLeave} onClick={onClick}>
+			<Tooltip title={tooltip}>
+				<FileCopyRoundedIcon className='copy_icon' />
+			</Tooltip>
 		</span>
 	);
 };
